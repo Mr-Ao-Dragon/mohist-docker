8b1cc2f41f28e2ede7b1357ca7e6c2d289efa6b2
敖律风 <x123456789fy@outlook.com>
2024-08-26 17:30:47 +0800

    update: 简化下载程序

--- go.mod
+++ go.mod
@@ -1,16 +1,5 @@
 module github.com/WHF-Studio/mohist-docker
 
-go 1.21.9
+go 1.23.0
 
-require (
-	github.com/Kenshin/curl v0.0.0-20160421052854-aeef514670e2
-	github.com/carlmjohnson/requests v0.23.5
-	github.com/urfave/cli/v2 v2.27.1
-)
-
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/net v0.15.0 // indirect
-)
+require github.com/Mr-Ao-Dragon/MCSL-Sync-Golang-SDK v0.0.0-20240826090002-a8a340b48125 // indirect
--- script/download-go/main.go
+++ script/download-go/main.go
@@ -1,97 +1,39 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"errors"
-	"io"
+	"github.com/Mr-Ao-Dragon/MCSL-Sync-Golang-SDK/get"
+	"github.com/Mr-Ao-Dragon/MCSL-Sync-Golang-SDK/info"
+	"github.com/Mr-Ao-Dragon/MCSL-Sync-Golang-SDK/setup"
 	"log"
-	"net/http"
 	"os"
-	"path/filepath"
-
-	"github.com/Kenshin/curl"
-	"github.com/carlmjohnson/requests"
+	"sort"
+	"strconv"
 )
 
-type pingRepose struct {
-	Message string `json:"message"`
-}
-type versions struct {
-	Versions []string `json:"versions"`
-}
-type buildList struct {
-	ProjectName    string   `json:"projectName"`
-	ProjectVersion string   `json:"projectVersion"`
-	Builds         []Builds `json:"builds"`
-}
-type Builds struct {
-	Number       int    `json:"number"`
-	GitSha       string `json:"gitSha"`
-	ForgeVersion string `json:"forgeVersion"`
-	FileMd5      string `json:"fileMd5"`
-	OriginUrl    string `json:"originUrl"`
-	Url          string `json:"url"`
-	CreatedAt    int64  `json:"createdAt"`
-}
-
 func main() {
-	version := os.Getenv("Version")
-	if version == "" {
-		log.Fatalf("error! \n%+v", errors.New("version is required"))
+	client := setup.InitSetupData(
+		"sync.mcsl.com.cn",
+		true,
+		"",
+		"mohist",
+		os.Getenv("MCVersion"),
+		"",
+		"/app",
+	)
+	data := new(info.CoreInfo)
+	data.GetCoreBuildListSingleMCVersion(*client)
+	strKey := make([]int, 0)
+	for k := range data.HistoryVersion {
+		numKey, err := strconv.Atoi(k)
+		if err != nil {
+			log.Panicf("无法对列表进行转换")
+		}
+		strKey = append(strKey, numKey)
 	}
-	pathRaw := os.Getenv("Path")
-	path := filepath.Clean(pathRaw)
-	if pathRaw == "" {
-		path = filepath.Clean("/app")
+	sort.Ints(strKey)
+	err := get.Download(*client, data.HistoryVersion[strconv.Itoa(strKey[len(strKey)-1])], "server.app")
+	if err != nil {
+		log.Fatalf("fail: %v", err)
 	}
 
-	log.Printf("ok! mohist %s will be download to: %s\n", version, path)
-	ping := new(pingRepose)
-	_ = requests.
-		URL("https://mohistmc.com").
-		Path("api/v2/ping").
-		ToJSON(&ping)
-	if ping.Message != "pong" {
-		log.Fatalf("error! \n%+v", errors.New("api offline"))
-	}
-	versionList := new(versions)
-	_ = requests.
-		URL("https://mohistmc.com").
-		Path("api/v2/projects/mohist").
-		ToJSON(&versionList)
-	for index, v := range versionList.Versions {
-		switch {
-		case version == v:
-			break
-		case index == len(versionList.Versions)-1:
-			log.Fatalf("error! \n%+v", errors.New("version not found"))
-		}
-	}
-	builds := new(buildList)
-	var maxNumber int
-	var maxBuild Builds
-	for _, build := range builds.Builds {
-		if build.Number > maxNumber {
-			maxNumber = build.Number
-			maxBuild = build
-		}
-	}
-	httpStatusCode, resp, err := curl.Get(maxBuild.OriginUrl)
-	if err != nil {
-		log.Fatalf("error! \n%+v", err)
-	}
-	if httpStatusCode != http.StatusOK {
-		log.Fatalf("error! \n%+v", errors.New("Failed to download: "+maxBuild.OriginUrl))
-	}
-	BodyBytes, _ := io.ReadAll(resp.Body)
-	if md5Obj := md5.New(); hex.EncodeToString(md5Obj.Sum(BodyBytes)) != maxBuild.FileMd5 {
-		log.Fatalf("error: file verify failed!\nlocal MD5 is: %s\nbut remote MD5 is: %s", hex.EncodeToString(md5Obj.Sum(nil)), maxBuild.FileMd5)
-	}
-	log.Printf("write file to: %s/server.jar\n", path)
-	err = os.WriteFile(path+"/server.jar", BodyBytes, 444)
-	if err != nil {
-		log.Fatalf("error! \n%+v", err)
-	}
-	return
 }
